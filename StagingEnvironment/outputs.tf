34eb261fcd1817a91ad9e4b5fa7287971b68d1c7
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-06-15 09:48:28 +0200

    Add files via upload

---
+++ StagingEnvironment/outputs.tf
@@ -0,0 +1,11 @@
+output "resource_group_name" {
+  value = module.staging.resource_group_name
+}
+
+output "environment" {
+  value = module.staging.environment
+}
+
+output "public_ip_address" {
+  value = module.staging.The_webserver_Public_ip
+}
